a611600f4ad5081beeb595c69a2acea1e0d4f36d
Alexander Blunck <alex@blunck.xyz>
2016-01-25 15:51:14 +0100

    Remoed unused logic from node server

--- server.js
+++ server.js
@@ -12,13 +12,6 @@ var express = require('express'),
 app.use(express.static(__dirname + '/dist'));
 
 /**
- * Return 404 for accidental api requests
- */
- app.all('/api/*', function (req, res) {
-     res.status(404).end();
- });
-
-/**
  * Route all other requests to angular
  */
 app.get('*', function (req, res) {
