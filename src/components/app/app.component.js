6fcd560918030ebaa5a5965b1eb7a4aaba92a51d
Alexander Blunck <alex@blunck.co>
2017-07-13 20:15:38 +0200

    Added icon to test vvg loading

--- src/components/app/app.component.js
+++ src/components/app/app.component.js
@@ -2,7 +2,15 @@
  * Component: App
  */
 
+import icon from '../../assets/icon.svg'
+
 class AppCtrl {
+
+    /* @ngInject */
+    constructor($element) {
+        const elem = $element[0].querySelector('.icon')
+        elem.innerHTML = icon
+    }
 
     $onInit() {
         this.name = 'starter-angular'
